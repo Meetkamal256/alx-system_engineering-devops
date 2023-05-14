bfe97099cd7aede4e7fcfd871f5b1281c3837607
meetkamal256 <meetkamal256@gmail.com>
2023-05-14 03:25:35 +0100

    go

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,25 +7,25 @@ import requests
 
 def count_words(subreddit, word_list, after="", count=[]):
     """count all words"""
-
+    
     if after == "":
         count = [0] * len(word_list)
-
+    
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     request = requests.get(url,
                            params={'after': after},
                            allow_redirects=False,
                            headers={'user-agent': 'bhalut'})
-
+    
     if request.status_code == 200:
         data = request.json()
-
+        
         for topic in (data['data']['children']):
             for word in topic['data']['title'].split():
                 for i in range(len(word_list)):
                     if word_list[i].lower() == word.lower():
                         count[i] += 1
-
+        
         after = data['data']['after']
         if after is None:
             save = []
@@ -34,7 +34,7 @@ def count_words(subreddit, word_list, after="", count=[]):
                     if word_list[i].lower() == word_list[j].lower():
                         save.append(j)
                         count[i] += count[j]
-
+            
             for i in range(len(word_list)):
                 for j in range(i, len(word_list)):
                     if (count[j] > count[i] or
@@ -46,7 +46,7 @@ def count_words(subreddit, word_list, after="", count=[]):
                         aux = word_list[i]
                         word_list[i] = word_list[j]
                         word_list[j] = aux
-
+            
             for i in range(len(word_list)):
                 if (count[i] > 0) and i not in save:
                     print("{}: {}".format(word_list[i].lower(), count[i]))
